c811afb1e872347a30cd9d8d382515dba8f754d2
kuangyeheng <kuangyeheng@gmail.com>
2017-01-19 14:55:30 +0800

    .

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var through = require('through2');
-var dot = require('dot');
+var doT = require('dot');
 var gutil = require('gulp-util');
 var path = require('path');
 var fs = require('fs');
@@ -9,16 +9,11 @@ var PLUGIN_NAME = 'gulp-dot-preparse';
 
 var opts = {};
 
-var transFn = function (fileContents,opts,cb) {
-	//do something with file contents
-	cb(fileContents);
-};
-
 opts.__proto__.injectDefChunk = function (defPaths) {
 	for (var i = 0; i < defPaths.length; i++) {
 		var defName = path.basename(defPaths[i],path.extname(defPaths[i]));
 		if (opts.__includes[defName]) {
-			gutil.log(gutil.colors.bold.red(path.basename(defPaths[i]) + '文件有重复，只会选取路径最浅的那个，也就是第一个'));
+			gutil.log(gutil.colors.bold.red(path.basename(defPaths[i]) + 'file is more than one ,it only use the first which matched by glob'));
 			continue;
 		}
 		opts.__includes[defName] = readData(defPaths[i]);
@@ -55,7 +50,7 @@ opts.__proto__.compile_to_UMD_module = function (modulename, template, def) {
 	compiled += defaultcompiled.toString().replace('anonymous', modulename);
 	return "(function(){" + compiled
 		   + "var itself=" + modulename + ", _encodeHTML=(" + doT.encodeHTMLSource.toString() + "(" + (settings.doNotSkipEncoded || '') + "));"
-		   + addexports(exports)
+		   + addExports(exports)
 		   + "if(typeof module!=='undefined' && module.exports) module.exports=itself;else if(typeof define==='function')define(function(){return itself;});else {"
 		   + this.__global + "=" + this.__global + "||{};" + this.__global + "['" + modulename + "']=itself;}}());";
 };
@@ -95,8 +90,6 @@ module.exports = function (options) {
 	
 	opts.injectDefChunk(defPaths);
 	
-	console.log(opts);
-	
 	return through.obj(function (file, encoding, cb) {
 		var _this = this;
 		if (file.isNull() || file.isDirectory()) {
@@ -113,16 +106,14 @@ module.exports = function (options) {
 			var fileName = path.basename(file.path);
 			var fileNameWithOutExt = path.basename(file.path,ext);
 			
-			if (/\.def(\.dot|\.jst)?$/.test(fileName)) {
-				opts.__includes[fileNameWithOutExt] = fileContents;
+			if (!/\.jst/.test(ext)) {
+				return cb(new gutil.PluginError(PLUGIN_NAME, 'File extname is not supported'));
 			}
 			
-			transFn(fileContents,opts, function  (fileContents) {
-				file.path = gutil.replaceExtension(file.path, '.js');
-				file.contents = new Buffer(fileContents);
-				_this.push(file);
-				cb();
-			});
+			file.path = gutil.replaceExtension(file.path, '.js');
+			file.contents = new Buffer(opts.compile_to_UMD_module(fileNameWithOutExt, fileContents));
+			_this.push(file);
+			cb();
 		}
 	});
 };
